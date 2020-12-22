2bd51fc0ab20ea8d332e6b6e22d014899a32b978
Falko <hfschoonewille@gmail.com>
2020-12-22 13:42:15 +0100

    finished the skeleton

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require '../vendor/autoload.php';
+require '../core/bootstrap.php';
+
+use App\Core\{Router, Request};
+
+Router::load('../app/routes.php')
+    ->direct(Request::uri(), Request::method());
